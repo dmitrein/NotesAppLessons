42c4f9b062a77d9e89c132099b1e1ebb56f93c18
dimmu <dimmudev@gmail.com>
2023-04-20 12:44:30 +0200

    prevStartScreen()

--- app/src/main/java/de/abyshkin/notesappmvvm/screens/Start.kt
+++ app/src/main/java/de/abyshkin/notesappmvvm/screens/Start.kt
@@ -2,11 +2,22 @@ package de.abyshkin.notesappmvvm.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import de.abyshkin.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
 @Composable
 fun StartScreen(navController: NavHostController) {
     Text(
         text = "Notes App Screen"
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun prevStartScreen(){
+    NotesAppMVVMTheme {
+        StartScreen(navController = rememberNavController())
+    }
 }
